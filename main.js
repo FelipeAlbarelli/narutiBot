28f2759f2747e34ead0ac9fe107b074e2698e408
Felipe Albarelii <f167197@dac.unicamp.br>
2020-09-27 16:29:51 -0300

    comentado tudo - teste

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-require('dotenv').config()
-const Twitter = require('twitter');
-const express = require('express')
-const configSch = require('./schedule');
-const functions = require('./functions');
+// require('dotenv').config()
+// const Twitter = require('twitter');
+// const express = require('express')
+// const configSch = require('./schedule');
+// const functions = require('./functions');
 
-const app = new Twitter({
-    consumer_key: process.env.API_KEY,
-    consumer_secret: process.env.API_SECRET_KEY,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-})
+// const app = new Twitter({
+//     consumer_key: process.env.API_KEY,
+//     consumer_secret: process.env.API_SECRET_KEY,
+//     access_token_key: process.env.ACCESS_TOKEN,
+//     access_token_secret: process.env.ACCESS_TOKEN_SECRET
+// })
 
-configSch(app);
+// configSch(app);
 
-app.stream('statuses/filter', {track: 'Naruti'},  function(stream) {
-    stream.on('data', function(tweet) {
-        console.log(`${tweet.text}, por ${tweet.user.screen_name}, capdato na stream`)
-        if (functions.isSocrates(tweet.user.screen_name)) {
-            functions.reply(tweet, "O que foi Sócrates?");
-        } else {
-            functions.reply(tweet, "Cada coisa né?");
-        }
-    });
-    stream.on('error', function(error) {
-      console.log(error);
-    });
-  });
+// app.stream('statuses/filter', {track: 'Naruti'},  function(stream) {
+//     stream.on('data', function(tweet) {
+//         console.log(`${tweet.text}, por ${tweet.user.screen_name}, capdato na stream`)
+//         if (functions.isSocrates(tweet.user.screen_name)) {
+//             functions.reply(tweet, "O que foi Sócrates?");
+//         } else {
+//             functions.reply(tweet, "Cada coisa né?");
+//         }
+//     });
+//     stream.on('error', function(error) {
+//       console.log(error);
+//     });
+//   });
 
-const app2 = express()
-const port = process.env.PORT || 3000;
+// const app2 = express()
+// const port = process.env.PORT || 3000;
 
-app2.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app2.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app2.listen(port, () => {
-  console.log(`Example app listening at port:${port}`)
-})
+// app2.listen(port, () => {
+//   console.log(`Example app listening at port:${port}`)
+// })
