d2fd0767493486b1950df0259bcf4f53cdd3dc6b
Felipe Albarelii <f167197@dac.unicamp.br>
2020-09-27 12:49:56 -0300

    usar .env para informações importantes

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
+require('dotenv').config()
 const Twitter = require('twitter');
-const env = require('./env');
 
 const app = new Twitter({
-    consumer_key: env.apiKey,
-    consumer_secret: env.apiSecreteKey,
-    access_token_key: env.accessToken,
-    access_token_secret: env.accessTokenSecret
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
 const reply = (tweet, text) => {
     const nameID = tweet.id_str;
-    const name = `@${tweet.user.screen_name}`
+    const name = `@${tweet.user.screen_name} `
     var res = {
         status:  name + text,
         in_reply_to_status_id: nameID
-    };
-    console.log(tweet);
+    }
     app.post('statuses/update', res,
       function(err, data, response) {
             console.log('data:', "*".repeat(20));
-            console.log(data)
+            console.log(`Respondendo ${data.in_reply_to_screen_name}`)
       }
     );
 }
 
 app.stream('statuses/filter', {track: 'Naruti'},  function(stream) {
     stream.on('data', function(tweet) {
+        console.log(`${tweet.text}, por ${tweet.user.screen_name}, capdato na stream`)
         reply(tweet, "Cada coisa né?");
     });
-  
     stream.on('error', function(error) {
       console.log(error);
     });
